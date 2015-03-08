221c7f3c5462d45677653ac0c5cfd2bd5c2a86d5
Jose Lezama Gonzalez <joseglego@gmail.com>
2015-03-08 12:28:51 -0430

    Agregado un Sencillo Dashboard

--- rails-angular/datawarriors/front-end/app/scripts/routes.js
+++ rails-angular/datawarriors/front-end/app/scripts/routes.js
@@ -3,23 +3,24 @@
 angular.module('datawarriors');
 
 angular.module('datawarriors').config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  '$locationProvider',
-  function ($stateProvider, $urlRouterProvider, $locationProvider) {
-     
-  $urlRouterProvider.otherwise("/");
-    $locationProvider.html5Mode(true);
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$locationProvider',
+    function ($stateProvider, $urlRouterProvider, $locationProvider) {
+	
+	$urlRouterProvider.otherwise("/");
+	//$locationProvider.html5Mode(true);
 
-  $stateProvider
-   .state('dashboard', {
-     url: '/',
-     templateUrl: "views/index.html"
-   });
+	$stateProvider
+	    .state('dashboard', {
+		url: '/',
+		templateUrl: "views/dashboard.html"
+	    });
 
-  $stateProvider
-   .state('category', {
-     url: '/category',
-     templateUrl: "views/category.html"
-   });
-  }]);
+	$stateProvider
+	    .state('category', {
+		url: '/category',
+		templateUrl: "views/category.html"
+	    });
+
+    }]);
