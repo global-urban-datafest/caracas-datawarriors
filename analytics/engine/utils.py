de6538e53fce39a2ae771f2f86605c898d179821
Ana <citricn@gmail.com>
2015-03-08 07:34:17 -0430

    Location and Category count

--- analytics/engine/utils.py
+++ analytics/engine/utils.py
@@ -1,13 +1,19 @@
 import unicodedata
+import re
 
-def preprocess(corpus, stopwords):
-    with open(stopword) as text:
-        dictionary = text.read().split()
-    corpus = [map(lambda x: x.split(), tweet) for tweet in corpus]
+def preprocess(corpus):
+    corpus = [x.split() for x in corpus]
     corpus = [map(lambda x: (unicodedata.normalize('NFKD', x)).encode('ASCII', 'ignore'), tweet)
                   for tweet in corpus]
     corpus = [filter(lambda x: len(x)>2, tweet) for tweet in corpus]
     corpus = [filter(lambda x: x.isalnum() or re.match(r'(#|@)\w+', x), tweet) for tweet in corpus]
-    corpus = [filter(lambda x: x not in dictionary, tweet) for tweet in corpus]
+    corpus = [' '.join(tweet) for tweet in corpus]
     return corpus
 
+def remove_stopwords(tweet, stopwords):
+    with open(stopwords) as text:
+        dictionary = text.read().split()
+    tweet = tweet.split()
+    tweet = filter(lambda x: x not in dictionary, tweet)
+    tweet = ' '.join(tweet)
+    return tweet
