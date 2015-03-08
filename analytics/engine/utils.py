ba7d7ac36f5e886e325f8f8b168d8b9dc1acd4f8
ML <emele18@gmail.com>
2015-03-08 02:56:34 -0430

    los archivos locales que faltaban

---
+++ analytics/engine/utils.py
@@ -0,0 +1,12 @@
+import unicodedata
+
+def preprocess(corpus, stopwords):
+    with open(stopword) as text:
+        dictionary = text.read().split()
+    corpus = [map(lambda x: (unicodedata.normalize('NFKD', x)).encode('ASCII', 'ignore'), tweet)
+                  for tweet in corpus]
+    corpus = [filter(lambda x: len(x)>2, tweet) for tweet in corpus]
+    corpus = [filter(lambda x: x.isalnum() or re.match(r'(#|@)\w+', x), tweet) for tweet in corpus]
+    corpus = [filter(lambda x: x not in dictionary, tweet) for tweet in corpus]
+    return corpus
+
