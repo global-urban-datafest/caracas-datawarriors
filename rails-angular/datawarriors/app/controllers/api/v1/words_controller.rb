2ca512f48a86e4fc67bb88448d6b5f04c473fc4f
Victor De Ponte <rdbvictor19@gmail.com>
2015-03-08 08:55:07 -0430

    Tweets controller fixed. Basic Categories and Words controller implemented.

--- rails-angular/datawarriors/app/controllers/api/v1/words_controller.rb
+++ rails-angular/datawarriors/app/controllers/api/v1/words_controller.rb
@@ -1,21 +1,19 @@
 class Api::V1::TweetsController < ApplicationController
 
   def index
-    params.require(:gov, :category)
-    # TODO: order by relevance
-    @tweets = Tweets.only(:id, :text, :created_at, :retweet_count, :sentiment, :gov)
-    render :status => 200, :json => @tweets.where(gov: params[:gov]).where(category_id: params[:category]).limit(20)
+    params.require(:category, :gov, :ordering)
+    @tweets = Tweets.only(:id, :text, :created_at, :neighborhood, :retweet_count, :gov, :similarity_id, :category_id)
+    render :status => 200, :json => @tweets.where(category_id: params[:category]).limit(20)
   end
 
 
   def sentiment
-    params.require(:gov, :category)
     none = -2
     neg = -1
     neu = 0
     pos = 1
 
-    all = Tweets.only(:id, :text, :created_at, :retweet_count, :sentiment, :gov).where(gov: params[:gov]).where(category_id: params[:category])
+    all = Tweets.only(:id, :text, :created_at, :neighborhood, :retweet_count, :gov, :similarity_id, :category_id)
 
     positive = all.where(:sentiment => pos).limit(20)
     negative = all.where(:sentiment => neg).limit(20)
